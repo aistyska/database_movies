4a81d1f68f3fb3bae1ce20c6d975c45459796d93
Aiste Laugalyte <aiste.laugalyte@gmail.com>
2020-03-04 09:20:22 +0200

    deleted commented code and added .gitignore

--- database/movie_db.py
+++ database/movie_db.py
@@ -49,30 +49,3 @@ def select_query(query, p = None):
     return None
 
 
-# # select_query("""SELECT movies.*, group_concat(actors.first_name ||\" \"|| actors.last_name ||\" - \"|| characters.character) FROM movies
-# # JOIN characters ON movies.movie_id = characters.movie_id
-# # JOIN actors ON characters.actor_id = actors.actor_id
-# # GROUP BY movies.movie_id""")
-# #
-# # select_query("""SELECT movies.*, group_concat(writers.first_name || \" \" || writers.last_name) FROM movies
-# # JOIN writing_crew ON movies.movie_id = writing_crew.movie_id
-# # JOIN writers ON writing_crew.writer_id = writers.writer_id
-# # GROUP BY movies.movie_id""")
-#
-# big = """SELECT studios.studio_name, studios.creation_date,
-# directors.first_name, directors.last_name,
-# group_concat(DISTINCT(writers.first_name || \" \" || writers.last_name)),
-# movies.title, movies.language, movies.release_date, movies.runtime,
-# group_concat(DISTINCT(actors.first_name ||\" \"|| actors.last_name ||\" - \"|| characters.character)),
-# box_office.budget, box_office.gross
-# FROM movies
-# JOIN studios ON studios.studio_id = movies.studio_id
-# JOIN directors ON directors.director_id = movies.director_id
-# JOIN writing_crew ON writing_crew.movie_id = movies.movie_id
-# JOIN writers ON writers.writer_id = writing_crew.writer_id
-# JOIN characters ON characters.movie_id = movies.movie_id
-# JOIN actors ON actors.actor_id = characters.actor_id
-# JOIN box_office ON box_office.movie_id = movies.movie_id
-# GROUP BY movies.movie_id
-# """
-# select_query(big)
